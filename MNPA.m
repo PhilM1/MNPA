c8c639ead1da27b389e4615dcab7259ac4d92f5c
Phil <pmasson993@gmail.com>
2020-03-03 14:20:11 -0500

    Finished PA

--- MNPA.m
+++ MNPA.m
@@ -1,5 +1,7 @@
 %MNPA for ELEC4700
 %Philippe Masson
+clear;
+close all;
 
 R1 = 1;
 Cap = 0.25;
@@ -34,13 +36,95 @@ F = [0;
      0;
      0;
      0;
-     Vin;
-     0];
+     0;
+     Vin];
+
+%DC Sweep
+Vin = -10;
+V3Array = [];
+V0Array = [];
+VinArray = [];
+Vcount = 1;
+while (Vin <= 10)
+    F = [0;
+    0;
+    0;
+    0;
+    0;
+    0;
+    Vin];
+    VDC = G\F;
+    V3Array(Vcount) = VDC(4);
+    V0Array(Vcount) = VDC(7)*R0/(R4+R0);
+    VinArray(Vcount) = Vin;
+    Vcount = Vcount + 1;
+    Vin = Vin + 1;
+end
+
+%plot
+figure();
+plot(VinArray, V0Array);
+title('V0 (DC)');
+xlabel('Vin (V)');
+ylabel('V0 (V)');
+figure();
+plot(VinArray, V3Array);
+title('V3 (DC)');
+xlabel('Vin (V)');
+ylabel('V3 (V)');
+
+
+%AC Sweep;
+VAC = (G+1i*2*pi*freq*C)\F;
+freq = 1;
+Vin = 10;
+V0ACArr = [];
+freqArr = [];
+gainArr = [];
+Vcount = 1;
+ while (freq <= 1e2)
+    VAC = (G+1i*2*pi*freq*C)\F;
+    V0ACArr(Vcount) = VAC(7)*R0/(R4+R0);
+    freqArr(Vcount) = freq;
+    gainArr(Vcount) = 20*log10(V0ACArr(Vcount)/Vin);
+    freq = freq + 1;
+    Vcount = Vcount + 1;
+ end
  
+ %plot  
+ figure();
+ plot(freqArr, V0ACArr);
+ title('V0 (AC)');
+ xlabel('Frequency (Hz)');
+ ylabel('V0 (V)');
  
- %DC Sweep
- VDC = G\F;
+ figure();
+ plot(freqArr, gainArr);
+ title('Gain (AC, 20dB per Decade)');
+ xlabel('Frequency (Hz)');
+ ylabel('Gain (dB)');
  
- %AC Sweep;
- VAC = (G+j*2*pi*freq*C)\F;
-     
+ %AC Sweep2;
+ CArr = normrnd(Cap,0.05,[100000,1]);
+ CArr = sort(CArr);
+ 
+ Vin = 10;
+ V0AC2Arr = [];
+ gain2Arr = [];
+ for c = 1:length(CArr)
+    VAC = (G+1i*pi*CArr(c))\F;     
+    V0AC2Arr(c) = VAC(7)*R0/(R4+R0);
+    gain2Arr(c) = 20*log10(V0AC2Arr(c)/Vin);
+ end
+ 
+ %plot
+ figure();
+ plot(CArr, gain2Arr);
+ title('Gain of C Perturbations (AC, 20dB per Decade)');
+ xlabel('C');
+ ylabel('Gain (dB)');
+ 
+ figure();
+ histogram(real(gain2Arr))
+ title('Gain Histogram of C Perturbations');
+ 
